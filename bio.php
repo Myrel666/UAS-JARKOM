734357aa98180b6e4cda4cb87560235ae7769d4d
Myrel666 <61135398+Myrel666@users.noreply.github.com>
2021-02-18 11:51:47 +0700

    Create bio.php

---
+++ bio.php
@@ -0,0 +1,35 @@
+< ?php
+    echo "<br>";
+    echo "Data UAS 2020/2021";
+    $tgl = date('l,d-m-Y');
+    echo "<br>";
+    
+    echo "<table width = 500 border =1>
+    <tr>
+        <td colspan=3><b>Biodata</b>
+        <br></br>
+        <br>$tgl;</td></tb>
+        <tr>
+            <td rowspan=4><img src=ega_profil.jpeg></td>
+            <td>nama:</td>
+            </td>:Ega ALmira</td>
+    </tr>
+    <tr>
+        <td>alamat:</td>
+        <td>Jl.Gadung V no.10</td>
+    </tr>
+        <tr>
+        <td>FB</td>
+        <td><a href=https://www.facebook.com/egaalmiraseph/>Ega Almira</td>
+    </tr>
+    <tr>
+        <td>Instagram</td>
+        <td><a href=https://www.instagram.com/egalmr/>Ega Almira</td>
+        <tr>
+        <td colspan=3>
+            <center>-EAS-</center>
+        </td>
+        </tr>
+    </tr>
+    </table>
+?>
